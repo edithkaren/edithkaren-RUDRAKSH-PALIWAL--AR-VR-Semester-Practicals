6ef3f6be282e2b2a874b92fd2ae8164fc2c4aa93
RudrakshPaliwal <75530058+edithkaren@users.noreply.github.com>
2022-03-03 11:14:30 +0530

    Add files via upload

---
+++ ARKit rudraksh/ARKit rudrakshTests/ARKit_rudrakshTests.swift
@@ -0,0 +1,34 @@
+//
+//  ARKit_rudrakshTests.swift
+//  ARKit rudrakshTests
+//
+//  Created by Student on 22/02/22.
+//  Copyright © 2022 SVIIT. All rights reserved.
+//
+
+import XCTest
+@testable import ARKit_rudraksh
+
+class ARKit_rudrakshTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
