2e33b977e2d803e523229e7b96c2b6c2121b35d7
RudrakshPaliwal <75530058+edithkaren@users.noreply.github.com>
2022-04-19 14:58:47 +0530

    Add files via upload

---
+++ TapGesture/TapGesture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TapGesture
+//
+//  Created by Student on 19/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
